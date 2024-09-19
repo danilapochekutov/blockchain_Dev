c065651cecb852dbaac4a7126123b3c33fa84cbd
danilapochekutov <danya270203@gamil.com>
2024-09-19 21:26:20 +0300

    added dev.js and  README.md

--- js/script.js
+++ js/script.js
@@ -5,10 +5,12 @@ import fullStack from "./modules/full-stack";
 import caseStudies from "./modules/case-studies";
 import clients from "./modules/clients";
 import animation from "./modules/wow";
+import dev from "./modules/dev";
 
 document.addEventListener("DOMContentLoaded", () => {
 	header();
 	fullStack();
+	dev();
 	caseStudies();
 	clients();
 	animation();
